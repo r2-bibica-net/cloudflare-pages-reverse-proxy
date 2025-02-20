7186b51a44fb4644b92eadca6f8ea6125d205a66
r2-bibica-net <r2@bibica.net>
2025-02-20 18:37:02 +0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,28 +1,5 @@
-export async function onRequest(context) {
-  try {
-    const url = new URL(context.request.url);
-    url.hostname = 'comment.bibica.net';
-    
-    const response = await fetch(url.toString(), {
-      method: context.request.method,
-      headers: {
-        'Host': 'comment.bibica.net'
-      }
-    });
-
-    return new Response(response.body, {
-      status: response.status,
-      headers: {
-        'Access-Control-Allow-Origin': '*'
-      }
-    });
-  } catch (error) {
-    return new Response(`Error: ${error.message}`, {
-      status: 500,
-      headers: {
-        'Content-Type': 'text/plain',
-        'Access-Control-Allow-Origin': '*'
-      }
-    });
-  }
+export function onRequest(context) {
+  return new Response("Hello World", {
+    headers: { "Content-Type": "text/plain" }
+  });
 }
